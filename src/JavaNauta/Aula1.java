70d2002b3df10f602227ef15298c0b6eb79d5d9d
Carol <acstorrea49@gmail.com>
2025-04-07 19:11:19 -0300

    Create Variable

--- src/JavaNauta/Aula1.java
+++ src/JavaNauta/Aula1.java
@@ -1,4 +1,9 @@
 package JavaNauta;
 
 public class Aula1 {
+
+    public static void main(String[] args) {  //Esse é o método principal, responsável pela execução do programa
+
+        System.out.println("Hello, World!");
+    }
 }
--- src/Variable.java
+++ src/Variable.java
@@ -1,4 +1,9 @@
 public class Variable {
+    /*
+    * O que é uma variável? Como declarar uma variável? Quais os tipos de variáveis?
+    *
+    *
+    * */
     public static void main(String[] args) {
         int age = 21;
         double alt = 1.60;
